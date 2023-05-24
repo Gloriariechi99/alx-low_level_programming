b81790513151ba921d347aaf633227dfff6e3e10
Gloriariechi99 <gloriariechi99@gmail.com>
2023-05-24 02:47:12 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+/**
+ * void jack_bauer(void) - prints every minute of the day of
+ * jack bauer starting from 00:00 to 23:59
+ * Rettun: void
+ */
+#include "main.h"
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
