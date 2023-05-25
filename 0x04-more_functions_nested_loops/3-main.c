28464ababb0c8a97fd4248e5014a813aa5edbbd9
Gloriariechi99 <gloriariechi99@gmail.com>
2023-05-25 06:20:06 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,10 @@
+/**
+ * main - cheks the code
+ * Return: Always 0
+ */
+#include "main.h"
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+/**
+ * print_numbers - prints the numbers from
+ * 0 to 9 followed by a new line using _putchar
+ * Return: Always 0
+ */
+#include "main.h"
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+
+	_putchar('\n');
+}
