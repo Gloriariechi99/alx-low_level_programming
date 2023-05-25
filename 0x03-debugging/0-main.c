d51526928934f0b9c07fe96f4f9f714b79b3c08e
Gloriariechi99 <gloriariechi99@gmail.com>
2023-05-25 02:09:58 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+/**
+ * main - tests if the integer is positive or negative
+ * Return: 0
+ */
+#include "main.h"
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
