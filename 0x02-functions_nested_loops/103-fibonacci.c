d0373a3ad64cd16899ac1577b76fa8a216c4200d
Gloriariechi99 <gloriariechi99@gmail.com>
2023-05-25 01:02:12 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+/**
+ * main - finds and prints the sum of the
+ * even-valued terms whose value does not exceed 4000000
+ * Return: Always 0
+ */
+#include <stdio.h>
+int main(void)
+{
+	int first_term = 1;
+	int second_term = 2;
+	int sum = 0;
+	int fib;
+
+	while (second_term <= 4000000)
+	{
+		if ((second_term % 2) == 0)
+		{
+			sum = sum + second_term;
+		}
+
+		fib = first_term + second_term;
+		second_term = fib;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
