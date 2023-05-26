e42cbdb5510d48f527cf8ff03de293904d418286
Gloriariechi99 <gloriariechi99@gmail.com>
2023-05-26 01:28:30 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+/**
+ * main - start of the program
+ * Return: Always 0
+ */
+#include <stdio.h>
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0 && (num % 5) != 0)
+		{
+			printf(" Fizz");
+			printf(" ");
+		}
+		else if ((num % 5) == 0 && (num % 3) != 0)
+		{
+			printf(" Buzz");
+		}
+		else if ((num % 3) == 0 && (num % 5) == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else
+		{
+			printf(" %d", num);
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
