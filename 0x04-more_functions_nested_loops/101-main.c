1f8ac1a4911da67db77289c5d8816acd899c90d8
Gloriariechi99 <gloriariechi99@gmail.com>
2023-05-26 02:50:31 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,19 @@
+/**
+ * main - check the code
+ * Return: always 0
+ */
+#include "main.h"
+int main(void)
+{
+	print_number(98);
+	_putchar('\n');
+	print_number(402);
+	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(0);
+	_putchar('\n');
+	print_number(-98);
+	_putchar('\n');
+	return (0);
+}
