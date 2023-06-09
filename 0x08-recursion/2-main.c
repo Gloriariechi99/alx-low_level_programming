ec09f6a841440368a004aa9536a196cbea7c5d2a
Gloriariechi99 <gloriariechi99@gmail.com>
2023-06-09 04:46:59 -0700

    function that returns the length of a string

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,14 @@
+/**
+ * main - checks the code
+ * Return: Always 0
+ */
+#include "main.h"
+#include <stdio.h>
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
+}
